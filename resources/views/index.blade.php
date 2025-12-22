ad31ba464772b824593ef0168d061ab228d802ba
Shahem Jahjah <shahemjahjah@gmail.com>
2025-12-22 12:13:46 +0100

    Setting up app w. docker, layouts, routes etc

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title')
+<h1>
+    List Of Tasks
+</h1>
+
+@section('content')
+<div>
+    @forelse ($tasks as $task )
+     <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id ])}}">{{ $task->title }}</a>
+     </div>
+    @empty
+     <div>No tasks!</div>
+    @endforelse
+
+@endsection
